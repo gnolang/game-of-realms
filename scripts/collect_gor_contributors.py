15b99f533291c5cc81b9807df38f5e104df13a44
waymobetta <jbroethke@gmail.com>
2023-07-26 08:10:19 -0700

    add pagination + formatting

--- scripts/collect_gor_contributors.py
+++ scripts/collect_gor_contributors.py
@@ -8,30 +8,45 @@ repos = [
     'gno-by-example',
 ]
 
-def write_to_file(repo,data):
+def write_to_file(repo, data, counter):
     f = open('gor_contributors.txt', 'a')
-    f.write(repo)
+
+    if counter == 0:
+        f.write(f'game of realms contributor list')
+        counter += 1
+
+    f.write(f'===============\r{repo}\r===============')
     f.write('\n\n') # formatting
     f.write('\n'.join(data))
     f.write('\n\n') # formatting
     f.close()
 
-def get_contributors(repo):
-    url = f'https://api.github.com/repos/gnolang/{repo}/pulls?state=closed'
+def get_contributors(repo, counter):
+    url = f'https://api.github.com/repos/gnolang/{repo}/pulls?state=closed&page=1'
 
     resp = requests.get(url)
 
-    # Ensure the request was successful
+    users = []
+    users_no_dupes = []
+
     if resp.status_code == 200:
         data = resp.json()
-        users = [item['user']['login'] for item in data]
-        users_no_dupes = list(dict.fromkeys(users))
-        write_to_file(repo,users_no_dupes)
+ 
+        # pagination
+        while 'next' in resp.links.keys():
+            resp = requests.get(resp.links['next']['url'])
+            data.extend(resp.json())
+            users = [item['user']['login'] for item in data]
+            users_no_dupes = list(dict.fromkeys(users))
     else:
         print(f"request failed with status code {resp.status_code}")
 
+    # final operation
+    write_to_file(repo, users_no_dupes, counter)
+
 if __name__ == '__main__':
-    print(f'[+] looping through {len(repos)} repos')
+    counter = 0
+    print(f'[+] collecting GoR contributors across {len(repos)} repos')
     for repo in repos:
-        get_contributors(repo)
-    print('[*] successfully fetched GoR contributors')
+        get_contributors(repo, counter)
+    print(f'[*] successfully collected GoR contributors')
